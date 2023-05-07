cdb659588909211d2b3e19b4abcd7e719431067f
Łukasz Plust <84743879+lukaszplust@users.noreply.github.com>
2023-05-07 11:32:48 +0200

    Add files via upload

--- lab5/main.m
+++ lab5/main.m
@@ -5,7 +5,11 @@ K_tab = [5,15,25,30];
 [XX, YY] = meshgrid(linspace(0,1,101), linspace(0,1,101));
 
 for K = K_tab
-
+    %K -> punkty pomiarowe
+    %x,y -> współrzedne pobrania próbek
+    %f -> wketor zawierajacy wartosci pobranych probek
+    %xp, yp -> wspolrzedne polozenia lazika w kolejnych chwilach pobierania
+    %probek
     [x, y, f, xp, yp] = lazik(K);
     
     sgtitle('K = ' + string(K));
@@ -21,16 +25,26 @@ for K = K_tab
     
     %wykres wartosci probek
     subplot(2,2,2);
+    %surf -> trojwymiarowa powierzchnia
     surf(reshape(x,K,K),reshape(y,K,K),reshape(f,K,K));
     shading flat % bez tego wyglada to lepiej XD
     title("Wartosci probek");
     xlabel("x[m]");
     ylabel("y[m]");
     zlabel("f(x,y)");
+    %wyswietlanie siatki na aktualnym wykresie
     grid on;
 
     %wykres interpolacji wielomianowej
-
+    
+    %polyfit wyznacza wartosci wspolczynnikow pm,n = liczba wezlow pobranych probek, czyli K^2
+    
+    %polyval2d wyznacza interpolowane wartosci funckji w dowolnych punktach
+    %badanego obszaru przy zastosowaniu interpolacji wielomianowej
+    %p -> wektor zawierajacy wartosci wspolczynnikow interpolacji
+    % XX, YY - macierze, zawierajace wspolrzedne punktow, w ktorych
+    % wyznaczane beda interpolowane wartosci funkcji
+    %FP - macierz zawierajaca interpolowane wartosci funkcji
     [p] = polyfit2d(x,y,f);
     [FP] = polyval2d(XX,YY,p);
 
@@ -45,7 +59,15 @@ for K = K_tab
     grid on;
 
     %interpolacja trygonometryczna
+    
+    %trygfit wyznacza wartosci wspolczynnikow tm,n
 
+    %trygval2d wyznacza interpolowane wartosci funckji w dowolnych punktach
+    %badanego obszaru przy zastosowaniu interpolacji trygonometrycznej
+    %p -> wektor zawierajacy wartosci wspolczynnikow interpolacji
+    % XX, YY - macierze, zawierajace wspolrzedne punktow, w ktorych
+    % wyznaczane beda interpolowane wartosci funkcji
+    %FT - macierz zawierajaca interpolowane wartosci funkcji
     [t] = trygfit2d(x,y,f);
     [FT] = trygval2d(XX,YY,t);
     
@@ -68,6 +90,7 @@ licznik = 1;
 for i = 5:45
 
     [x, y, f,xp, yp] = lazik(i);
+    %wygenerowanie siatki punktow XX oraz YY
     [XX, YY] = meshgrid(linspace(0,1,101), linspace(0,1,101));
     
     %wielomianowa
@@ -106,7 +129,3 @@ xlabel('Liczba punktów pomiarowych K')
 ylabel('Maksymalne wartości różnicy interpolowanych funkcji')
 title('Wykres zbieżności dla interpolacji trygonometrycznej')
 saveas(gcf, 'ZAD_2_trygonometryczna.png','png');
-
-
-
-
