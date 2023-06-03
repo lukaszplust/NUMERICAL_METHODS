1bbc2aa6a7eda0fca25c9dc61f7829455d8d348d
Łukasz Plust <84743879+lukaszplust@users.noreply.github.com>
2023-06-03 15:50:45 +0200

    Dodac txt i ogarnac 1 zadanko

--- LAB7/main.m
+++ LAB7/main.m
@@ -31,7 +31,7 @@ x = 5:50:10^4;
 
 
 % METODA PROSTOKATOW
-
+tic;
 error = []; %przechowywana wartosc bledu
 
 for N = 5:50:10^4% N przyjmuje wartosci od 5 do 10^10 z krokiem 50
@@ -53,6 +53,7 @@ for N = 5:50:10^4% N przyjmuje wartosci od 5 do 10^10 z krokiem 50
         sum = sum + dx * awaria(mid);
     end
     error = [error, abs(sum - P_ref)];%odejmuje obliczona wartosc referencyjna od obliczonej sumy
+    prostokat_time = toc;
 end
 
 figure();
@@ -65,7 +66,7 @@ saveas(gcf, 'metoda_prostokatow.png')
 
 
 %METODA TRAPEZOW
-
+tic;
 error = [];
 
 for N = 5:50:10^4
@@ -83,6 +84,7 @@ for N = 5:50:10^4
         sum = sum + var;%sumuje wartosci pol trapezow dla wszystkich przedzialow
     end
     error = [error, abs(sum - P_ref)];%odejmuje obliczona wartosc referencyjna od obliczonej sumy
+    trapez_time = toc;
 end
 
 figure();
@@ -94,7 +96,7 @@ saveas(gcf, 'metoda_trapezow.png')
 
 
 % METODA SIMPSONA
-
+tic;
 error = [];
 
 for N = 5:50:10^4
@@ -117,6 +119,7 @@ for N = 5:50:10^4
     end
     sum = sum * dx / 6; % dx/6 -> wspolczynnik skalujacy charakterystyczny dla metody Simpsona
     error = [error, abs(sum - P_ref)];%odejmuje obliczona wartosc referencyjna od obliczonej sumy
+    simpson_time = toc;
 end
 figure();
 loglog(x, error);
@@ -124,6 +127,73 @@ title('Bład - metoda Simpsona');
 xlabel('Liczba przedziałow');
 ylabel('Wartosc błedu');
 saveas(gcf, 'metoda_simpsona.png')
+
+% MONTE CARLO
+tic;
+error = [];
+fmin = 0;
+fmax = awaria(5);%fmax jest to wartosc funkcji awaria dla argumentu = 5
+
+% N -> liczba wylosowanych punktow
+% b -> szerokosc przedzialu w ktorym probkowane sa punkty
+
+
+for N = 5:50:10^4
+
+    N_1 = 0; %liczba punktow ktore spelniaja warunek(punkty pod krzywa)
+    
+    for i = 1:N%generowanie losowych punktow i sprawdzanie czy spelnia warunek
+
+        %probkowanie losowe
+        x_monte = rand() * b;%zakres losowania -> [0,b]
+        y = rand() * fmax; % zakres losowania -> [0,fmax]
+
+        %sprawdzam czy losowa wartosc y znajduje się miedzy fmin a awaria(x)
+        %jesli tak to oznacza, ze wylosowany punkt (x,y) spelnia warunek
+        
+        %poprzez takie probkowanie i sprawdzanie warunku obliczane jest
+        %przyblizenie wartosci calki
+        if y >= fmin && y <= awaria(x_monte)
+            N_1 = N_1 + 1;
+        end
+    end
+    
+    % (N_1 / N) -> prawdopodobienstwo, ze losowo wybrany punkt spelnia
+    % warunek
+
+    %((N_1 / N)) * b -> średnia dlugosc przedziału, w ktorym punkty
+    %spelniaja warunek
+
+    % ((N_1 / N)) * b * fmax -> skalowanie sredniej dlugosci przedzialu
+    % przez maksymalna wartosc funkcji, co jest przyblizona wartoscia całki
+    % zakres [b. fmax]
+    P = (N_1 / N) * b * fmax;%przyblizona wartosc całki
+    error = [error, abs(P - P_ref)];
+    monte_carlo_time = toc;
+end
+
+figure();
+loglog(x, error);
+title('Bład - metoda Monte-Carlo');
+xlabel('Liczba przedziałow');
+ylabel('Wartosc błedu');
+saveas(gcf, 'metoda_monte_carlo.png')
+
+%wykresy czasu
+
+N = 10^7;
+figure();
+
+X = categorical({'Prostokątow','Trapezow','Simpsona','Monte Carlo'});
+X = reordercats(X,{'Prostokątow','Trapezow','Simpsona','Monte Carlo'});%ustawienie w kolejnosci
+
+bar(X,[prostokat_time,trapez_time,simpson_time,monte_carlo_time])
+
+xlabel('Metoda')
+ylabel('Czas [s]')
+title('Czas trwania metod')
+saveas(gcf, 'wykresy_czasow.png','png');
+
 
 %funkcja awarii
 
--- LAB7/zadanie_b.m
+++ LAB7/zadanie_b.m
@@ -17,3 +17,33 @@ axis equal
 % z = glebokosc(x,y); % wyznaczanie glebokosci jeziora w punkcie (x,y),
 % gdzie x i y sa losowane
 %------------------------------------------
+N = 10^5;
+
+x_min = 0;
+x_max = 100;
+
+y_min = 0;
+y_max = 100;
+
+z_min = -50;
+z_max = 0;
+
+
+points = 0; % points = punkty nad krzywa
+
+for i = 1:N
+    x = (x_max - x_min).*rand(N,1) + xmin;
+    y = (y_max - y_min).*rand(N,1) + ymin;
+    z = (z_max - z_min).*rand(N,1) + zmin;
+     
+    func = glebokosc(x,y);%wartosci funkcji w danym punkcie
+
+    if z >= func
+        points = points +1;
+    end
+end
+value = (points/N) * ((x_max - x_min) * (y_max - y_min) * (z_max - z_min))
+
+
+
+
